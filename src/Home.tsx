3c01925e55c816b4560a712fd6254334c9eaffdd
wassim dhiba <wassimdhibaa@gmail.com>
2022-08-12 17:05:11 +0200

    pong

--- src/Home.tsx
+++ src/Home.tsx
@@ -7,15 +7,9 @@ function Home(){
             <h1 id="bandeau"> Welcome to our FT_Transcendance !</h1>
             <p id ="to-play">If you want to play to a simple pong please use the map 1.</p>
             <div id="choice">
-            <form id="pong1" action="/pong" data-style="font-family: OCR A Std;">
-                <input type="submit" value="simple map" />
-            </form>
-            <form id="pong2" action="/pong2">
-                <input type="submit" value="night map" />
-            </form>
-            <form id="pong3" action="/pong3">
-                <input type="submit" value="tennis map" />
-            </form>
+            <button id="pong1" className="bouton" type="button" onClick={(e) => { e.preventDefault(); window.location.href='/pong'; }}> simple pong </button>
+            <button id="pong2" className="bouton" type="button" onClick={(e) => { e.preventDefault(); window.location.href='/pong2'; }}> space pong </button>
+            <button id="pong3" className="bouton" type="button" onClick={(e) => { e.preventDefault(); window.location.href='/pong3'; }}> tennis pong </button>
             </div>
         </div>
     );
@@ -26,13 +20,22 @@ window.addEventListener("load", function(){
     let pong1: any = this.document.querySelector("#pong1");
     let pong2: any = this.document.querySelector("#pong2");
     let pong3: any = this.document.querySelector("#pong3");
+
     all.style.textAlign= "center";
+    pong1.style.fontFamily = "OCR A Std";
+    pong2.style.fontFamily = "OCR A Std";
+    pong3.style.fontFamily = "OCR A Std";
+    pong1.style.fontSize = "18px";
+    pong2.style.fontSize = "18px";
+    pong3.style.fontSize = "18px";
+    
     pong1.style.marginRight = "2%";
     pong1.style.marginTop = "10%";
     pong1.style.marginBottom = "37%";
     pong2.style.marginRight = "2%";
     pong2.style.marginBottom = "37%";
     pong3.style.marginRight = "2%";
+
     all.style.backgroundColor= "#4682B4";
 });
 export default Home;
